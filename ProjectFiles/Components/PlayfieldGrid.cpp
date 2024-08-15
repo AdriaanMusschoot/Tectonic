2da13a232915717a359007dd0ff3078c872ac2d8
AdriaanMusschoot <adriaan.musschoot@gmail.com>
2024-08-15 11:57:13 +0200

    add random generation

--- ProjectFiles/Components/PlayfieldGrid.cpp
+++ ProjectFiles/Components/PlayfieldGrid.cpp
@@ -1,7 +1,31 @@
 #include "PlayfieldGrid.h"
 
-tct::PlayfieldGridComponent::PlayfieldGridComponent(amu::GameObject* ownerObjectPtr, int rows, int cols)
+#include <random>
+#include <chrono>
+
+tct::TectonicGridComponent::TectonicGridComponent(amu::GameObject* ownerObjectPtr, unsigned int rows, unsigned int cols, unsigned int highestNr)
 	: Component(ownerObjectPtr)
+	, m_Rows{ rows }
+	, m_Cols{ cols }
+	, m_HighestNumber{ highestNr }
 	, m_GridVec(rows * cols)
 {
+	FillGrid();
+}
+
+void tct::TectonicGridComponent::FillGrid()
+{
+	//auto seed{ std::chrono::steady_clock::now().time_since_epoch().count() };
+	std::random_device randDev{};
+	unsigned int seed{ randDev() };
+
+	std::default_random_engine randEng{ seed };
+
+	std::uniform_int_distribution<unsigned int> dist{ 1, m_HighestNumber };
+
+	for (auto& cell : m_GridVec)
+	{
+		cell = dist(randDev);
+	}
+
 }
--- ProjectFiles/Components/PlayfieldGrid.h
+++ ProjectFiles/Components/PlayfieldGrid.h
@@ -8,18 +8,23 @@
 namespace tct
 {
 
-	class PlayfieldGridComponent final : public amu::Component
+	class TectonicGridComponent final : public amu::Component
 	{
 	public:
-		PlayfieldGridComponent(amu::GameObject* ownerObjectPtr, int rows, int cols);
-		virtual ~PlayfieldGridComponent() override = default;
+		TectonicGridComponent(amu::GameObject* ownerObjectPtr, unsigned int rows, unsigned int cols, unsigned int highestNr);
+		virtual ~TectonicGridComponent() override = default;
 
-		PlayfieldGridComponent(PlayfieldGridComponent const&) = delete;
-		PlayfieldGridComponent(PlayfieldGridComponent&&) = delete;
-		PlayfieldGridComponent& operator=(PlayfieldGridComponent const&) = delete;
-		PlayfieldGridComponent& operator=(PlayfieldGridComponent&&) = delete;
+		TectonicGridComponent(TectonicGridComponent const&) = delete;
+		TectonicGridComponent(TectonicGridComponent&&) = delete;
+		TectonicGridComponent& operator=(TectonicGridComponent const&) = delete;
+		TectonicGridComponent& operator=(TectonicGridComponent&&) = delete;
 	private:
-		std::vector<int> m_GridVec{};
+		unsigned int const m_Rows{};
+		unsigned int const m_Cols{};
+		unsigned int const m_HighestNumber{};
+		std::vector<unsigned int> m_GridVec{};
+
+		void FillGrid();
 	};
 
 }
