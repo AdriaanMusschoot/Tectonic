b239cca747816f0957179c0513f100d719d3e3f3
AdriaanMusschoot <adriaan.musschoot@gmail.com>
2024-08-18 21:11:59 +0200

    added resources and type alias for uint max

--- ProjectFiles/Configuration.h
+++ ProjectFiles/Configuration.h
@@ -3,9 +3,12 @@
 
 #include <string>
 #include <memory>
+#include <numeric>
 
 namespace tct
 {
+	using direction = std::pair<int, int>;
+
 	namespace img
 	{
 		static std::string const NR1{ "1.png" };
@@ -15,13 +18,28 @@ namespace tct
 		static std::string const NR5{ "5.png" };
 	}
 
-	namespace dir
+	namespace fnt
 	{
 
-		static std::pair<int, int> const LEFT{ 1, 0 };
-		static std::pair<int, int> const RIGHT{ -1, 0 };
-		static std::pair<int, int> const UP{ 0, -1 };
-		static std::pair<int, int> const DOWN{ 0, 1 };
+		static std::string const LINGUA{ "Lingua.otf" };
+
+	}
+
+	namespace dir
+	{
+		static int constexpr NR_OF_DIRECTIONS{ 4 };
+
+		static std::pair<int, int> constexpr LEFT{ -1, 0 };
+		static std::pair<int, int> constexpr RIGHT{ 1, 0 };
+		static std::pair<int, int> constexpr UP{ 0, -1 };
+		static std::pair<int, int> constexpr DOWN{ 0, 1 };
+
+	}
+
+	namespace max
+	{
+
+		static unsigned int constexpr UIN{ std::numeric_limits<unsigned int>::max() };
 
 	}
 
